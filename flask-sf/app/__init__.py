8ff26153f6109aa61d2fc390f0b46b1ad18efb7c
hayamakazuyuki <hayama@sfinter.com>
2021-12-01 15:15:04 +0900

    refactor:config.py

--- flask-sf/app/__init__.py
+++ flask-sf/app/__init__.py
@@ -11,19 +11,20 @@ from .errors.handlers import errors
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object('config.BaseConfig')
-    
+    if app.config['ENV'] == 'production':
+        app.config.from_object('config.ProductionConfig')
+
+    elif app.config['ENV'] == 'testing':
+        app.config.from_object('config.TestingConfig')
+
+    else:
+        app.config.from_object('config.DevelopmentConfig')
+
     db.init_app(app)
     login_manager.init_app(app)
     admin.init_app(app)
     login_manager.login_view = 'staff.login'
     login_manager.login_message = False
-
-
-    @app.route('/')
-    def index():
-        this_env = app.config['ENV']
-        return this_env
 
     app.register_blueprint(contractor)
     app.register_blueprint(customer)
--- flask-sf/config.py
+++ flask-sf/config.py
@@ -10,9 +10,8 @@ class ProductionConfig(BaseConfig):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
-class DevelopmentConfig(BaseConfig):
-    DEBUG = True
-
 class TestingConfig(BaseConfig):
     TESTING = True
     
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
